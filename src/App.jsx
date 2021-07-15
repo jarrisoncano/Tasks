0013c224b904ff5aa6f57b2728273d911b433dde
Jarrison Cano <jarrison.cano@hotmail.com>
2021-07-15 14:53:57 -0500

    :alembic: experiments with husky v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import 'Firebase/Firebase'
+import React from 'react'
 import Main from './components/main/Main'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Footer from './components/footer/Footer'
