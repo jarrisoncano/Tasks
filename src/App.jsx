7cce0c371f8c92cb24401233ab31e67c0cc743a9
Jarrison Cano <jarrison.cano@hotmail.com>
2021-07-15 14:18:23 -0500

    :alembic: experiments with husky v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'Firebase/Firebase'
-
 import React from 'react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-
 import Main from './components/main/Main'
 import Footer from './components/footer/Footer'
 
