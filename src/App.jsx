9f0858bff4ada08075bdcc528df2b4224af5bd8d
Jarrison Cano <jarrison.cano@hotmail.com>
2021-07-15 14:52:53 -0500

    :alembic: experiments with husky v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'Firebase/Firebase'
 import React from 'react'
+import 'Firebase/Firebase'
 import Main from './components/main/Main'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Footer from './components/footer/Footer'
 
 export default function App() {
